01adff268a6494ba9a9c408f630d0aae60193df3
Raman Novakhatski <romabelarus@mail.ru>
2023-06-17 12:51:40 +0300

    addItemToCart test created

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.qameta.allure.selenide.LogType;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.logging.Level;
+
+import static com.codeborne.selenide.Browsers.CHROME;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void start() {
+        SelenideLogger.addListener("allure", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+                .enableLogs(LogType.BROWSER, Level.ALL));
+
+        Configuration.browser =CHROME;
+    }
+
+    @BeforeMethod
+    public void goToUrl() {
+        open("https://www.saucedemo.com/");
+    }
+}
