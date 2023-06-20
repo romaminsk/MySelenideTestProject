0400b36b3a4d64e66c90b0288f4a74d2df35a917
Raman Novakhatski <romabelarus@mail.ru>
2023-06-20 21:16:01 +0300

    data provider created

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.SelenideElement;
+import org.example.models.UserData;
 import org.example.steps.LoginSteps;
 import org.example.steps.ProductsSteps;
+import org.example.utils.JsonReader;
 import org.example.utils.Waiters;
 import org.testng.annotations.Test;
 
@@ -9,15 +11,15 @@ public class SauceDemoTest extends BaseTest {
     LoginSteps loginSteps = new LoginSteps();
     ProductsSteps productsSteps = new ProductsSteps();
 
-    @Test(enabled = false)
-    public void loginTest() {
-        loginSteps.login("standard_user", "secret_sauce");
+    @Test(dataProvider = "userData", dataProviderClass = JsonReader.class)
+    public void loginTest(UserData userData) {
+        loginSteps.login(userData.getUsername(), userData.getPassword());
         loginSteps.verifyLogin("Products");
     }
 
-    @Test
-    public void addItemToCart() {
-        loginSteps.login("standard_user", "secret_sauce");
+    @Test(dataProvider = "userData", dataProviderClass = JsonReader.class)
+    public void addItemToCart(UserData userData) {
+        loginSteps.login(userData.getUsername(), userData.getPassword());
         SelenideElement cheapestProduct = productsSteps.findCheapestPrice();
         String productName = cheapestProduct.find(".inventory_item_name").getText();
         productsSteps.addProductToCart(cheapestProduct);
