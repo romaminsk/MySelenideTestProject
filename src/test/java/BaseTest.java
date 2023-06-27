9df1eaecf1a4ad9e375e3a1b3c1e548e7a670ccd
Raman Novakhatski <romabelarus@mail.ru>
2023-06-27 20:58:39 +0300

    selenoid added

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,10 +2,11 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.qameta.allure.selenide.LogType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 
@@ -18,14 +19,15 @@ public class BaseTest {
     private final String URL = bundle.getString("path_to_url");
 
     @BeforeClass
-    public void start() {
-        SelenideLogger.addListener("allure", new AllureSelenide()
-                .screenshots(true)
-                .savePageSource(true)
-                .enableLogs(LogType.BROWSER, Level.ALL));
-
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = CHROME;
+    @Parameters({"startType", "browser", "version"})
+    public void start(String startType,
+                      @Optional("browser") String browser,
+                      @Optional("version") String version) {
+        if (startType.equals("local")) {
+            startLocal();
+        } else if (startType.equals("selenoid")) {
+            startSelenoid(browser, version);
+        }
     }
 
     @BeforeMethod
@@ -37,5 +39,35 @@ public class BaseTest {
     public void clearData() {
         clearBrowserLocalStorage();
         clearBrowserCookies();
+    }
+
+    public static void startLocal() {
+        SelenideLogger.addListener("allure", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+                .enableLogs(LogType.BROWSER, Level.ALL));
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = CHROME;
+    }
+
+    public static void startSelenoid(String browser, String version) {
+        SelenideLogger.addListener("allure", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+                .enableLogs(LogType.BROWSER, Level.ALL));
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.browser = browser;
+
+        Map<String, Boolean> options = new HashMap<>();
+        options.put("enableVNC", true);
+        options.put("enableVideo", false);
+        options.put("enableLog", true);
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName(browser);
+        capabilities.setVersion(version);
+        capabilities.setCapability("selenoid:options", options);
+        Configuration.browserCapabilities = capabilities;
     }
 }
